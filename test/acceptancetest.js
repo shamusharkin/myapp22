afe4d3f58c40c2ebd58d7dbd974afa018aecc8ad
Shamus Harkin <shim50@hotmail.com>
2017-06-19 16:24:44 +0100

    updated wording on index.js

--- test/acceptancetest.js
+++ test/acceptancetest.js
@@ -15,7 +15,6 @@ AfterSuite((I) => {
 Scenario('IH:Tests1: Check that I can see Announcement on the home page as a header for our Announcements', (I) => {
 	I.amOnPage('/');
 	I.see('Express');
-  I.see('Welcome to Express');
 
   I.amOnPage('/page2')
   I.see('Shamus & Aimee Page')
